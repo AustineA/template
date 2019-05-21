a96eae4e55e7a6d53f5378db71d980eedc19e9a2
Austine Amah <austine.amah@gmail.com>
2019-05-21 06:31:00 +0100

    permitted square_meters in posts params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -78,7 +78,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:price, :duration, :street,:lga,:state,:area, :title, :purpose, :use_of_property, :type_of_property, :bedrooms, :bathrooms, :toliets, :description, :video_link )
+      params.require(:post).permit(:price, :duration, :street,:lga,:state,:area, :title, :purpose, :use_of_property, :type_of_property, :bedrooms, :bathrooms, :toliets, :description, :video_link,:square_meters )
     end
 
 end
