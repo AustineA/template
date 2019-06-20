7ce728d5418dfbc57f4a99c5ef09a4f8ca28d909
Austine Amah <austine.amah@gmail.com>
2019-06-20 05:41:54 +0100

    Change bathrooms column to bedrooms in post request table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_121826) do
+ActiveRecord::Schema.define(version: 2019_06_20_043510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2019_06_17_121826) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "bathrooms"
+    t.integer "bedrooms"
     t.index ["user_id"], name: "index_post_requests_on_user_id"
   end
 
