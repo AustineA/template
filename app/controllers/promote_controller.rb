c688a904a899751c740af40d269639cc630d9cbb
Austine Amah <austine.amah@gmail.com>
2019-08-14 14:12:00 +0100

    Refactored promote controller

--- app/controllers/promote_controller.rb
+++ app/controllers/promote_controller.rb
@@ -8,37 +8,66 @@ class PromoteController < ApplicationController
     post = Post.find_by_permalink(params[:id])
     subscription = current_user.subscription
     boost = params[:promote][:boost].to_i if params[:promote][:boost].present?
-    priority = params[:promote][:priority] if params[:promote][:priority].present?
+    priority = params[:promote][:priority].to_i if params[:promote][:priority].present?
     current_boost = post.boost
     current_priority = post.priority
     remaining_boost = subscription.boost
     remaining_priorities = subscription.priorities
     score = post.score
     message = { priority: "", boost: "" }
+    listing = params[:promote][:type]
     
-    if ( priority && ( priority <= max_priority ) && (remaining_priorities >= priority) && ( current_priority + priority  <= max_priority ) )
-      new_priority = current_priority + priority
-      post.update_attributes(priority: new_priority, score: score + priority, promotion_updated_at: Time.now)
-      subscription.update_attributes(priorities: remaining_priorities - priority)
-      priority_count = current_user.posts.where("priority > ?", 0).count
-      current_user.update_attributes(priority_count: priority_count)
-      message[:priority] = "success"
+
+
+    case 
+        when listing == "BOOST"
+          boost_listing( boost, max_boost, remaining_boost, current_boost, score, post, subscription )
+        when listing == "PRIORITY"
+          priority_listing( priority, max_priority, remaining_priorities, current_priority, score, post, subscription, current_priority )
+        else
+          render "Unknown plan"		
     end
-
-    
-    updated_post = Post.find_by_permalink(params[:id])
-
-
-    if ( boost && ( boost <= max_boost ) && (remaining_boost >= boost) && ( current_boost + boost  <= max_boost ) && updated_post.priority > 0)
-      new_boost = current_boost + boost
-      updated_score = updated_post.score
-      updated_post.update_attributes(boost: new_boost, score: updated_score + boost, promotion_updated_at: Time.now)
-      subscription.update_attributes(boost: remaining_boost - boost)
-      boost_count = current_user.posts.where("boost > ?", 0).count
-      current_user.update_attributes(boost_count: boost_count)
-      message[:boost] = "success"
-    end
-
-    render json: message
   end
+
+
+  private
+
+  def priority_listing( priority, max_priority, remaining_priorities, current_priority, score, post, subscription )
+    if remaining_priorities > 0
+      if ( priority <= max_priority  && ( current_priority + priority  <= max_priority ) )
+        new_priority = current_priority + priority
+        post.update_attributes(priority: new_priority, score: score + priority, promotion_updated_at: Time.now)
+        subscription.update_attributes(priorities: remaining_priorities - priority)
+        priority_count = current_user.posts.where("priority > ?", 0).size
+        current_user.update_attributes(priority_count: priority_count)
+      else
+        render json: { message: "You can only set 1 priority listing for this post" }
+      end
+    else
+      render json: { message: "You've ran out priority listings" }
+    end
+  end
+
+
+  def boost_listing( boost, max_boost, remaining_boost, current_boost, score, post, subscription, current_priority )
+    if remaining_boost > 0
+      if current_priority > 1
+        if ( boost <= max_boost  && ( current_boost + priority  <= max_boost ) )
+          new_boost = current_boost + boost
+          post.update_attributes(boost: new_boost, score: score + boost, promotion_updated_at: Time.now)
+          subscription.update_attributes(boost: remaining_boost - boost)
+          boost_count = current_user.posts.where("boost > ?", 0).size
+          current_user.update_attributes(boost_count: boost_count)
+        else
+          render json: { message: "You can only set 4 priority boosts for this post" }
+        end
+      else
+        render json: { message: "You must have priority listing set before you can boost this post" }
+      end
+    else
+      render json: { message: "You've ran out priority boosts" }
+    end
+  end
+
+
 end
